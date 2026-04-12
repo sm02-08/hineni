289dca11d29baa5d48dcd6c0ba640e38bdc65091
sm02-08 <130806123+sm02-08@users.noreply.github.com>
2026-04-12 22:42:42 +0000

    i dont know web dev.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("DyslexicFont").addEventListener("click", function() { // create a dyslexic font button that changes every word here to comic sans 
     document.body.style.fontFamily = "'Comic Neue', serif";
 });
+
+document.getElementById("ReturnDefault").addEventListener("click", function() { // create a dyslexic font button that changes every word here to comic sans 
+    document.body.style.fontFamily = "'Playwrite Ireland', Rubik";
+});
