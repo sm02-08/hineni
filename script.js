046872369d588608188075ad34c7520cbac0f06f
sm02-08 <130806123+sm02-08@users.noreply.github.com>
2026-04-12 03:52:55 +0000

    added title and body text!

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("DyslexicFont").addEventListener("click", function() { // create a dyslexic font button that changes every word here to comic sans 
+    document.body.style.fontFamily = "'Comic Neue', serif";
+});
